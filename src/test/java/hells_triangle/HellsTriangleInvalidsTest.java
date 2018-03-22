e21736aca096a2d84b19ffa6b0b3c0331abef347
Carlos Lira <carlosliracl@gmail.com>
2018-03-22 19:48:12 -0300

    remove printStackTraces on TestCase

--- src/test/java/hells_triangle/HellsTriangleInvalidsTest.java
+++ src/test/java/hells_triangle/HellsTriangleInvalidsTest.java
@@ -26,19 +26,19 @@ public class HellsTriangleInvalidsTest {
 		int[][] triangle_2 = { { 4 }, { 9 }, { 7, 1, 9 }, { 2, 4, 9, 9 } };
 		int[][] triangle_3 = { { 1 }, { 5, 6 }, { 8, 4 } };
 		int[][] triangle_4 = { { 3, 3 }, { 2, 0, 0 }, { 3, 8, 7, 0 } };
-		int[][] triangle_5 = { {  } };
+		int[][] triangle_5 = { {} };
 
-		return Arrays.asList(new Object[][] { { triangle_1, 26 }, { triangle_2, 30 }, { triangle_3, 17 },
-				{ triangle_4, 21 }, { triangle_5, 11 } });
+		return Arrays.asList(new Object[][] { 
+			{ triangle_1, 26 }
+			, { triangle_2, 30 }
+			, { triangle_3, 17 }
+			, { triangle_4, 21 }
+			, { triangle_5, 11 }
+		});
 	}
 
 	@Test(expected = IllegalTriangleException.class)
 	public void testSumTriangleWithException() {
-		try {
-			HellsTriangleUtils.validate(triangle);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
+		HellsTriangleUtils.validate(triangle);
 	}
 }
